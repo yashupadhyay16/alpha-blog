2468232f6a73a9c4f59c19c3f571b1eeea6b5482
Yash Upadhyay <upadhyayyash923@gmail.com>
2020-07-28 18:10:50 +0530

    update article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
     
     def show
+        
         @article = Article.find(params[:id])
         
     end
@@ -10,6 +11,9 @@ class ArticlesController < ApplicationController
     def new
         @article = Article.new
     end
+    def edit
+        @article = Article.find(params[:id])
+    end    
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))
         if @article.save
@@ -19,5 +23,14 @@ class ArticlesController < ApplicationController
             render 'new'
         end
     end
+    def update
+        @article=Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice]="Article was updared sucessfully"
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end    
 
 end
