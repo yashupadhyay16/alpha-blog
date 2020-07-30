d1f903b82aa48e4d9a37646e081a7450d9a15fc2
Yash Upadhyay <upadhyayyash923@gmail.com>
2020-07-31 01:27:29 +0530

    edit user pages  or modify

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
     
-    before_action :set_user,only: [:edit, :update]
+    before_action :set_user,only: [:edit, :update, :show]
     def new
         @user = User.new
     end 
@@ -26,7 +26,10 @@ class UsersController < ApplicationController
         else
             render 'edit'
         end        
-    end    
+    end 
+    def show
+        
+    end   
 
     private
     
