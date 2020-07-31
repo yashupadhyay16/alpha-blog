03a2b648224de519466063fd92a5e1791ba05c10
Yash Upadhyay <upadhyayyash923@gmail.com>
2020-07-31 17:43:34 +0530

    login and logout user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,26 @@
 class SessionsController < ApplicationController
+    
     def new
+    
     end
 
     def create
+        
+        user = User.find_by(email: params[:session] [:email].downcase)
+        if user && user.authenticate(params[:session] [:password])
+            session[:user_id] = user.id
+            flash[:notice] ="logged in successfully"
+            redirect_to user
+        else
+            flash.now[:alert] ="Enter email/password is incorrect"
+            render 'new'
+            
+        end        
     end
 
     def destroy
+        session[:user_id] = nil
+        flash[:notice]="Logout sucessfully"
+        redirect_to root_path
     end            
 end    
