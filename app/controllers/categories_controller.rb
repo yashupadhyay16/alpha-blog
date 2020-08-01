02d083b81322e7656874d4417ed39bd8f3cf667f
Yash Upadhyay <upadhyayyash923@gmail.com>
2020-08-01 18:55:32 +0530

    validation test using integration testing

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,26 +1,36 @@
 class CategoriesController < ApplicationController
 
+    # before_action :set_category, only: [:show] 
+   
     def new
         @category=Category.new
     end    
+    def create
+        @category = Category.new(category_params)
+        if @category.save
+            flash[:notice] = "Category was successfully created"
+            redirect_to @category 
+        else
+            render 'new'
+        end       
+    end 
     
+
     def index
     
     end
 
     def show
-    
+        @category = Category.find(params[:id])
     end 
-    def create
-        @category = Category.new(category_params)
-        if @category.save
-            flash[:notice] = "Category was successfully created"
-            redirect_to @category
-        end    
-    end   
-
+     
+    private
 
     def category_params
         params.require(:category).permit(:name)
     end
+
+    # def set_category
+        
+    # end
 end
