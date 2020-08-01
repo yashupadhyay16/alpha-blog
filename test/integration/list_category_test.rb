699514708b2fcd26e2a48f1eba161c49ce349535
Yash Upadhyay <upadhyayyash923@gmail.com>
2020-08-01 22:09:12 +0530

    index view for category and pagination and integration test

---
+++ test/integration/list_category_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class ListCategoryTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @category = Category.create(name: "Sports")
+    category = Category.create(name: "Travel")
+  end
+
+  test "should show categories listing" do
+    get '/categories'
+    assert_select "a[href=?]", category_path(@category), text: @category.name
+    assert_select "a[href=?]", category_path(@category2), text: @category2.name
+  end
+end
